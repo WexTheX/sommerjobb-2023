6da18636d65be12dcc4330417218885f1817e264
WexTheX <wexthex@gmail.com>
2023-07-04 09:56:56 +0200

    solcar 3d update

--- Servolab/solcelle 3D/solcelle_3d/solcelle_3d.ino
+++ Servolab/solcelle 3D/solcelle_3d/solcelle_3d.ino
@@ -16,20 +16,19 @@ const int SERVO_PIN_BOT = 10;
 unsigned long prevMil = 0;
 
 int NPR = 0; // Values for photoresistors (North, South, East, West)
-int SPR = 0; // Note these are names, if they acutally align with magnetic poles depends on how the physical structure is set up.
+int SPR = 0; // Note these are only names, if they acutally align with magnetic poles depends on set up.
 int EPR = 0;
 int WPR = 0;
 
 int servoPosTop = 90; // Set starting position of servos
 int servoPosBot = 90;
 
-enum servoState { MIDDLE, LEFT, RIGHT }; // TODO change ifelse to enum
+enum servoState { MIDDLE, LEFT, RIGHT }; // Enum for motor turn direction
 servoState servoTop = MIDDLE;
 servoState servoBot = MIDDLE;
 
 // Declare functions
 void updateSunPosition();
-void updateServoPosition();
 void updateServoPositionTop();
 void updateServoPositionBot();
 
@@ -42,9 +41,10 @@ void setup() {
 
 void loop() {
   updateSunPosition();
-  if(millis() - prevMil > 100){ // Only move servo within a given interval
+  if(millis() - prevMil > 100){ // Only move servo within a given interval to avoid too rapid movement
 
-    updateServoPosition();
+    updateServoPositionTop();
+    updateServoPositionBot();
     prevMil = millis();
   }
 }
@@ -55,39 +55,58 @@ void updateSunPosition(){ // Get light intensity values
   EPR = analogRead(EPR_PIN);
   WPR = analogRead(WPR_PIN);
 
-  /* Serial.print("N: "); Serial.print(NPR);
+  Serial.print("N: "); Serial.print(NPR);
   Serial.print(" S: "); Serial.print(SPR);
   Serial.print(" E: "); Serial.print(EPR);
-  Serial.print(" W: "); Serial.println(WPR); */
+  Serial.print(" W: "); Serial.println(WPR);
+
+  if((NPR > EPR+50) || (WPR > SPR+50)){ // If the light is stronger on the North/West side (+50 is buffer to avoid constant shifting)
+    servoTop = LEFT;
+  } else if ((NPR+50 < EPR) || (WPR+50 < SPR)){ // If the light is stronger on the South/East side
+    servoTop = RIGHT;
+  } else {
+    servoTop = MIDDLE;
+  }
+
+  if((NPR > WPR+50) || (EPR > SPR+50)){ // If the light is stronger on the North/East side
+    servoBot = LEFT;
+  } else if ((NPR+50 < WPR ) || (EPR+50 < SPR)){ // If the light is stronger on the South/West side
+    servoBot = RIGHT;
+  } else {
+    servoBot = MIDDLE;
+  }
 }
 
-void updateServoPosition(){ // Combine servo update functions
-  updateServoPositionTop();
-  updateServoPositionBot();
-}
-
-void updateServoPositionTop(){ // Update position of top servo
-  if((NPR+50 > EPR) && (WPR+50 > SPR)){ // If the light is stronger on the North/West side (with buffer)
-    servoPosTop -= 1;
-  } else if ((NPR < EPR+50) && (WPR < SPR+50)){ // If the light is stronger on the South/East side (with buffer)
-    servoPosTop += 1;
+void updateServoPositionTop(){ // Move top servo
+  switch(servoTop){
+    case MIDDLE:
+      break;
+    case RIGHT:
+      servoPosTop += 1; // Move 1 degree in positive direction
+      break;
+    case LEFT:
+      servoPosTop -= 1; // Move 1 degree in negative direction
   }
 
   servoPosTop = constrain(servoPosTop, 0, 180); // Makes sure position is within servos range
-  //Serial.print("Top"); Serial.println(servoPosTop);
+  Serial.print("Top"); Serial.println(servoPosTop);
   
   topservo.write(servoPosTop);
 }
 
-void updateServoPositionBot(){
-  if((NPR+50 > SPR) && (EPR+50 > WPR)){ // If the light is stronger on the North/East side (with buffer)
-    servoPosBot += 1;
-  } else if ((NPR < SPR+50) && (EPR < WPR+50)){ // If the light is stronger on the South/West side (with buffer)
-    servoPosBot -= 1;
+void updateServoPositionBot(){ // Move bottom servo
+  switch(servoBot){
+    case MIDDLE:
+      break;
+    case RIGHT:
+      servoPosBot += 1; // Move 1 degree in positive direction
+      break;
+    case LEFT:
+      servoPosBot -= 1; // Move 1 degree in negative directions
   }
 
   servoPosBot = constrain(servoPosBot, 0, 180); // Make sure position is within servos range
-  //Serial.print("Bot: "); Serial.println(servoPosBot);
+  Serial.print("Bot: "); Serial.println(servoPosBot);
 
   botservo.write(servoPosBot);
 }
